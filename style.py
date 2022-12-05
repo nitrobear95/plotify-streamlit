979eab6eb390c731ee32f790f33461d0ffe2ab2e
Sarah Blackburn <sblackburn_4@hotmail.com>
2022-12-05 11:09:16 +0000

    adding main app  py files

---
+++ style.py
@@ -0,0 +1,59 @@
+
+
+def get_css():
+    return """
+<style>
+
+element.style.css-1lh9kog.e1fqkh3o10 {
+    width:35%;
+}
+
+div.css-hxt7ib {
+    padding-top:2rem;
+}
+
+div.css-5m2qup {
+    gap: 1.5rem;
+}
+
+
+div.css-1ux96mu {
+    opacity: 0%;
+}
+
+button.css-1szhu3u {
+    background-color: black;
+    color: white;
+    padding: 0.4rem 1rem;
+    font-stretch: expanded;
+    width: 100%;
+    font-size: larger;
+}
+
+.css-heo2h5 {
+    color: white;
+}
+
+div.css-19qr612 {
+    color: black;
+}
+
+div.st-bx {
+    color: white;
+}
+
+div.st-cz {
+    background-color: black;
+}
+
+div.css-12oz5g7 {
+    padding: 3rem 1rem 10rem
+}
+
+.css-1v0mbdj {
+    width: 60%;
+
+}
+
+<style>
+"""
